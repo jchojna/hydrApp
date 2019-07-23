e0309d393f02bf7ea170e5245997325a1438a0f3
Jakub Chojna <jakub.chojna@gmail.com>
2019-07-24 01:08:56 +0200

    feat: JS basic functionality with local storage support

--- src/js/main.js
+++ src/js/main.js
@@ -12,3 +12,35 @@ if ('serviceWorker' in navigator) {
   });
 }
 
+const addGlassButton = document.querySelector('.app__button--js-add');
+const removeGlassButton = document.querySelector('.app__button--js-remove');
+const glassCounterContainer = document.querySelector('.glass__counter--js');
+
+const getGlassAmount = () => {
+  if (localStorage.getItem('glassCounter')) {
+    return parseInt(localStorage.getItem('glassCounter'));
+  } else {
+    localStorage.setItem('glassCounter', glassCounter);
+    return 0;
+  }
+}
+
+const updateCounter = (value) => {
+  glassCounterContainer.innerHTML = value;
+}
+
+const handleCounter = (command) => {
+  let glassCounter = getGlassAmount();
+  if (command === 'up') {
+    glassCounter === 99 ? false : glassCounter++;
+  }
+  if (command === 'down') {
+    glassCounter === 0 ? false : glassCounter--;
+  }
+  localStorage.setItem('glassCounter', glassCounter);
+  updateCounter(glassCounter);
+}
+
+updateCounter(getGlassAmount());
+addGlassButton.addEventListener('click', () => handleCounter('up'));
+removeGlassButton.addEventListener('click', () => handleCounter('down'));
