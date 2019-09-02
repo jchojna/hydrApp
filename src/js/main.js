638b16460257dfdf7c90d661995ad0a24764711f
Jakub Chojna <jakub.chojna@gmail.com>
2019-09-02 18:39:55 +0200

    feat: history of water consumption

--- src/js/main.js
+++ src/js/main.js
@@ -17,69 +17,66 @@ if ('serviceWorker' in navigator) {
 const addGlass = document.querySelector('.app__button--js-add');
 const removeGlass = document.querySelector('.app__button--js-remove');
 const counter = document.querySelector('.glass__counter--js');
+const historyList = document.querySelector('.history__list--js');
 
 /********** FUNCTIONS **********/
 
+// create hydrApp date key
 const setDateKey = (obj) => {
   const prefix = 'hydrApp-';
   const date = obj.toISOString().slice(0,10);
   return prefix.concat(date);
 }
 
+// create key value pair in localStorage
 const setNewKeyValue = (key, value) => {
   if ( !localStorage.getItem(key) ) {
     localStorage.setItem(key, value);
   };
 }
 
-
+const getHydrappKeys = () => {
+  // create array of hydrApp localStorage keys
+  const regex = /hydrApp-/;
+  return Object
+  .keys(localStorage)
+  .filter(key => regex.test(key))
+  .sort((a,b) => a<b);
+}
 
 const setCounter = () => {
   let date = new Date();
   let dateKey = setDateKey(date);
-  const regex = /hydrApp-/;
 
   setNewKeyValue(dateKey, 0);
 
+  const hydrappKeys = getHydrappKeys();
+  const oldestKey = hydrappKeys[hydrappKeys.length-1];
 
-
-
-  // create array of hydrApp localstorage keys
-  const hydrappKeys = Object
-    .keys(localStorage)
-    .filter(key => regex.test(key));
-
-  const sortedDates = hydrappKeys
-    .map(key => key.match(/[0-9]/g)
-    .join(''))
-    .sort((a,b) => a - b);
-  const oldestKey = sortedDates[0];
-
-  // add missing dates
+  // autocomplete missing keys
   if ( hydrappKeys.length > 1 ) {
-
-    while (setDateKey(date).match(/[0-9]/g).join('') !== oldestKey) {
+    while (setDateKey(date) !== oldestKey) {
       date.setDate( date.getDate() - 1 );
       const prevDateKey = setDateKey(date);
       setNewKeyValue(prevDateKey, 0);
     }
+  }
+  counter.innerHTML = localStorage.getItem(dateKey);
+}
 
+const setHistory = () => {
+  const hydrappKeys = getHydrappKeys();
+
+  for (const key of hydrappKeys) {
+    const value = localStorage.getItem(key);
+    historyList.innerHTML += `
+    <li>
+      <span>${key}</span>
+      <span>${value}</span>
+    </li>
+    `;
   }
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-  counter.innerHTML = localStorage.getItem(dateKey);
 }
 
 const updateCounter = (e) => {
@@ -99,5 +96,6 @@ const updateCounter = (e) => {
 
 
 setCounter();
+setHistory();
 addGlass.addEventListener('click', updateCounter);
 removeGlass.addEventListener('click', updateCounter);
