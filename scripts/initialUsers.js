2af57a9119db215c198d74a82ba99f302776a6b8
jchojna <jakub.chojna@gmail.com>
2020-03-13 00:27:21 +0100

    Update 2020-03-12T23:27:19.014Z

---
+++ scripts/initialUsers.js
@@ -0,0 +1,37 @@
+module.exports = [
+  {
+    name: 'Anna',
+    age: 29,
+    weight: 58,
+    height: 176,
+    dateCreated: 'February 01, 2020 23:30:05'
+  },
+  {
+    name: 'Agnieszka',
+    age: 28,
+    weight: 75,
+    height: 165,
+    dateCreated: 'February 03, 2020 15:32:56'
+  },
+  {
+    name: 'Aleksander',
+    age: 28,
+    weight: 70,
+    height: 174,
+    dateCreated: 'February 05, 2020 12:07:23'
+  },
+  {
+    name: 'Jakub',
+    age: 32,
+    weight: 67,
+    height: 181,
+    dateCreated: 'February 06, 2020 14:45:13'
+  },
+  {
+    name: 'Jakub C',
+    age: 32,
+    weight: 67,
+    height: 181,
+    dateCreated: 'February 06, 2020 14:45:45'
+  }
+]
