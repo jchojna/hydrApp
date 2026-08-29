3c348af3add461f85dd2555077a62839918149d7
Jakub-Chojna <jakub.chojna@gmail.com>
2026-08-29 21:08:27 +0200

    chore: revert login form

--- app/[lng]/page.tsx
+++ app/[lng]/page.tsx
@@ -1,3 +1,9 @@
-export default function Home() {
-  return null
+import { redirect } from "next/navigation"
+
+import { getCurrentUser } from "@/lib/dal/user"
+
+export default async function Home() {
+  const currentUser = await getCurrentUser()
+
+  redirect(currentUser ? "/dashboard" : "/signin")
 }
